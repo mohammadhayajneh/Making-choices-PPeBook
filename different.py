1dbc7d4d73fc348a433d2ec26e60d17ba8c32204
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-14 12:27:39 -0500

    Add files via upload

---
+++ different.py
@@ -0,0 +1,15 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def different(a,b):
+	'''(number,number)->bool
+
+        return True if a and b are different values, or false otherwise.
+
+        >>>different(10,1)
+        Treu
+        >>>different(0,0)
+        False
+        '''
+	return a!=b
+
+>>> 
